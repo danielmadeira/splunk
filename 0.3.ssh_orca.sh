e434b76e073c5db6c6d3ec7526afc7e3d695fe03
Daniel Mdadeira <daniel.madeira@yahoo.co.uk>
2022-09-20 12:56:12 +0100

    Update 0.3.ssh_orca.sh

--- 0.3.ssh_orca.sh
+++ 0.3.ssh_orca.sh
@@ -10,5 +10,6 @@ echo IP
 read ip
 echo PORT
 read port
+echo " you are about to connect to your orca instance. Once you are conneced in there, please cd /tmp and run ./1.1.packages.sh as ansible user "
 
 ssh -p "$port" -i ~/.orca/id_rsa ansible@"$ip"

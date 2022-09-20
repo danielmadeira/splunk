63fa66a2a55d89c1c1b0fdbad9761470ed5409c9
Daniel Mdadeira <daniel.madeira@yahoo.co.uk>
2022-09-20 13:08:35 +0100

    Update 2.4.rm_problematic_files_from_diag_apps.sh

--- 2.4.rm_problematic_files_from_diag_apps.sh
+++ 2.4.rm_problematic_files_from_diag_apps.sh
@@ -1,4 +1,5 @@
 cd /tmp/myapps
+echo " removing outputs.conf indexes.conf web.conf authentication.conf authorize.conf distsearch.conf limits.conf server.conf inputs.conf from all you previous selected apps "
 find . -name outputs.conf -exec rm -rf {} \;
 find . -name indexes.conf  -exec rm -rf {} \;
 find . -name web.conf   -exec rm -rf {} \;
@@ -8,3 +9,4 @@ find . -name distsearch.conf  -exec rm -rf {} \;
 find . -name limits.conf  -exec rm -rf {} \;
 find . -name server.conf  -exec rm -rf {} \;
 find . -name inputs.conf -exec rm -rf {} \;
+echo " next script to run is 2.5.look_for_other_confs.sh if needed - this is an alternative script if you need to remove other configs, such as macros.conf datamodels.conf"

85457aff40899b8194a19ac62e25aaf6b41c20f0
Daniel Mdadeira <daniel.madeira@yahoo.co.uk>
2022-09-20 13:18:40 +0100

    Update 2.6.look_if_all_trouble_files_were_removed.sh

--- 2.6.look_if_all_trouble_files_were_removed.sh
+++ 2.6.look_if_all_trouble_files_were_removed.sh
@@ -10,3 +10,7 @@ find . -name server.conf  -exec ls {} \;
 find . -name inputs.conf -exec ls {} \;
 
 echo " if you get no results on screen before this message, it means that the apps you are about to copy to your test instance are clear of outputs indexes web authentication authorize distsearch limits server inputs conf files "
+echo " you are now loggin is as splunk user, so please run 3.1.stop_chown_start.sh "
+
+sudo su splunk
+cd /tmp

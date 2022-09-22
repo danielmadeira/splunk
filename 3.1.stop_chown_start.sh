0f19e781b6ecaefe1d46370837332cb2edf5f4c7
Daniel Mdadeira <daniel.madeira@yahoo.co.uk>
2022-09-22 09:57:14 +0100

    Update 3.1.stop_chown_start.sh

--- 3.1.stop_chown_start.sh
+++ 3.1.stop_chown_start.sh
@@ -3,6 +3,7 @@ sudo chown -R splunk: /tmp/myapps
 sudo chown -R splunk: /opt/splunk
 mkdir /tmp/original_untouched_apps_splunk
 cp -rf /opt/splunk/etc/apps/* /tmp/original_untouched_apps_splunk
+rm -rf /opt/splunk/etc/apps/*
 cp -rf /tmp/myapps/* /opt/splunk/etc/apps/
 cp /tmp/Splunk_ITSI_NFR* /opt/splunk/etc/licenses/enterprise/Splunk_ITSI_NFR_FY23\ 2H.xml.lic
 sudo chown -R splunk: /opt/splunk
